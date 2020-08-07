dc092e1e83a632d4d5456224ccb92a7539ba61e6
Ippei Kowada <blue-oceans.0420@outlook.jp>
2020-08-07 18:39:37 +0900

    内容はViews.pyでタグはIndex.htmlに入れるんだよ

--- LeamingDjango/LeamingDjango/settings.py
+++ LeamingDjango/LeamingDjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'HelloDjangoApp'
 ]
 
 # Middleware framework
@@ -101,8 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ja'
+TIME_ZONE = 'Asia/Tokyo'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
