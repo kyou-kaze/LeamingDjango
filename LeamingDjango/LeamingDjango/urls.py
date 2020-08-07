c6333276407000e253e4359757503b4184a34136
Ippei Kowada <blue-oceans.0420@outlook.jp>
2020-08-07 19:46:00 +0900

    継承を利用すると内容がシンプルに

--- LeamingDjango/LeamingDjango/urls.py
+++ LeamingDjango/LeamingDjango/urls.py
@@ -26,5 +26,7 @@ urlpatterns = [
     # Uncomment the next line to enable the admin:
     #path('admin/', admin.site.urls)
     url(r'^$', HelloDjangoApp.views.index, name='index'),
-    url(r'^home$', HelloDjangoApp.views.index, name='home')
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
+    url(r'^about$', HelloDjangoApp.views.about, name='about')
+
 ]
